f5d64fece5b23ffc04b7777cd13cb1101e84aecc
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-10-10 18:04:39 +0200

    added reshape()

--- cudarray/base.py
+++ cudarray/base.py
@@ -46,3 +46,21 @@ def transpose(a):
     a_trans.shape = (a.shape[1], a.shape[0])
     a_trans.transposed = True
     return a_trans
+
+
+def reshape(a, newshape):
+    size = a.size
+    if isinstance(newshape, int):
+        newshape = (newshape,)
+    newsize = np.prod(newshape)
+    if size != newsize:
+        if newsize < 0:
+            # negative newsize means there is a -1 in newshape
+            newshape = list(newshape)
+            newshape[newshape.index(-1)] = -size // newsize
+            newshape = tuple(newshape)
+        else:
+            raise ValueError('cannot reshape %s to %s' % (a.shape, newshape))
+    a_reshaped = a.view()
+    a_reshaped.shape = newshape
+    return a_reshaped
