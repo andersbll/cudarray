3282a91872e44ad78f0e3514d70305aab6c28d60
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-10-14 23:32:59 +0200

    conv and pool operations are now classes
    This maked the interface simpler because we don't have to arguments
    like padding and striding for all function calls. More importantly,
    the user does not have to consider bookkeeping data like the mask
    used for pooling.

--- cudarray/nnet/conv.py
+++ cudarray/nnet/conv.py
@@ -2,84 +2,82 @@ import cudarray as ca
 from ..cudarray_wrap import nnet as wrap
 
 
-def conv_bc01(imgs, filters, padding, strides, convout=None):
-    b, c, img_h, img_w = imgs.shape
-    f, c_filters, filter_h, filter_w = filters.shape
+class ConvBC01(object):
+    def __init__(self, padding, strides):
+        self.padding = padding
+        self.strides = strides
 
-    convout_h = (img_h + 2 * padding[0] - filter_h) / strides[0] + 1
-    convout_w = (img_w + 2 * padding[1] - filter_w) / strides[1] + 1
-    convout_shape = (b, f, convout_h, convout_w)
-
-    if c != c_filters:
-        raise ValueError('channel mismatch')
-
-    if imgs.dtype != filters.dtype:
-        raise ValueError('dtype mismatch')
-
-    if convout is None:
-        convout = ca.empty(convout_shape, dtype=imgs.dtype)
-    else:
-        if convout.shape != convout_shape:
-            raise ValueError('convout.shape does not match result')
-        if convout.dtype != imgs.dtype:
+    def fprop(self, imgs, filters, convout=None):
+        b, c, img_h, img_w = imgs.shape
+        f, c_filters, filter_h, filter_w = filters.shape
+        if c != c_filters:
+            raise ValueError('channel mismatch')
+        if imgs.dtype != filters.dtype:
             raise ValueError('dtype mismatch')
 
-    wrap._conv_bc01_matmul(
-        imgs._data, filters._data, b, c, f, (img_h, img_w),
-        (filter_h, filter_w), padding, strides, convout._data
-    )
-    return convout
+        convout_shape = self.output_shape(imgs.shape, f, (filter_h, filter_w))
+        if convout is None:
+            convout = ca.empty(convout_shape, dtype=imgs.dtype)
+        else:
+            if convout.shape != convout_shape:
+                raise ValueError('convout.shape does not match result')
+            if convout.dtype != imgs.dtype:
+                raise ValueError('dtype mismatch')
+        wrap._conv_bc01_matmul(
+            imgs._data, filters._data, b, c, f, (img_h, img_w),
+            (filter_h, filter_w), self.padding, self.strides, convout._data
+        )
+        return convout
 
+    def bprop(self, imgs, filters, convout_d, to_filters=True, to_imgs=True,
+              filters_d=None, imgs_d=None):
+        b, c, img_h, img_w = imgs.shape
+        f, c_filters, filter_h, filter_w = filters.shape
+        b_convout, f_convout, convout_h, convout_w = convout_d.shape
+        img_shape = (img_h, img_w)
+        filter_shape = (filter_h, filter_w)
+        if b != b_convout:
+            raise ValueError('batch mismatch')
+        if f != f_convout:
+            raise ValueError('filter mismatch')
+        if c != c_filters:
+            raise ValueError('channel mismatch')
 
-def conv_bc01_bprop_filters(imgs, convout_d, filter_shape, padding, strides,
-                            filters_d=None):
-    b, c, img_h, img_w = imgs.shape
-    b_convout, f, convout_h, convout_w = convout_d.shape
-    filters_shape = (f, c) + filter_shape
-
-    if b != b_convout:
-        raise ValueError('batch mismatch')
-
-    if imgs.dtype != convout_d.dtype:
-        raise ValueError('dtype mismatch')
-
-    if filters_d is None:
-        filters_d = ca.empty(filters_shape, dtype=imgs.dtype)
-    else:
-        if filters_d.shape != filters_shape:
-            raise ValueError('filters_d.shape does not match result')
-        if filters_d.dtype != imgs.dtype:
+        if imgs.dtype != filters.dtype != convout_d.dtype:
             raise ValueError('dtype mismatch')
 
-    wrap._conv_bc01_matmul_bprop_filters(
-        imgs._data, convout_d._data, b, c, f, (img_h, img_w), filter_shape,
-        padding, strides, filters_d._data
-    )
-    return filters_d
+        if to_filters:
+            if filters_d is None:
+                filters_d = ca.empty(filters.shape, dtype=filters.dtype)
+            else:
+                if filters_d.shape != filters.shape:
+                    raise ValueError('filters_d.shape does not match result')
+                if filters_d.dtype != filters.dtype:
+                    raise ValueError('dtype mismatch')
+            wrap._conv_bc01_matmul_bprop_filters(
+                imgs._data, convout_d._data, b, c, f, img_shape,
+                filter_shape, self.padding, self.strides, filters_d._data
+            )
 
+        if to_imgs:
+            if imgs_d is None:
+                imgs_d = ca.empty(imgs.shape, dtype=imgs.dtype)
+            else:
+                if imgs_d.shape != imgs.shape:
+                    raise ValueError('imgs_d.shape does not match result')
+                if imgs_d.dtype != imgs.dtype:
+                    raise ValueError('dtype mismatch')
+            wrap._conv_bc01_matmul_bprop_imgs(
+                filters._data, convout_d._data, b, c, f, img_shape,
+                filter_shape, self.padding, self.strides, imgs_d._data
+            )
 
-def conv_bc01_bprop_imgs(filters, convout_d, img_shape, padding, strides,
-                         imgs_d=None):
-    f, c, filter_h, filter_w = filters.shape
-    b, convout_f, convout_h, convout_w = convout_d.shape
-    imgs_shape = (b, c,) + img_shape
+        return filters_d, imgs_d
 
-    if f != convout_f:
-        raise ValueError('channel mismatch')
-
-    if filters.dtype != convout_d.dtype:
-        raise ValueError('dtype mismatch')
-
-    if imgs_d is None:
-        imgs_d = ca.empty(imgs_shape, dtype=filters.dtype)
-    else:
-        if imgs_d.shape != imgs_shape:
-            raise ValueError('imgs_d.shape does not match result')
-        if imgs_d.dtype != filters.dtype:
-            raise ValueError('dtype mismatch')
-
-    wrap._conv_bc01_matmul_bprop_imgs(
-        filters._data, convout_d._data, b, c, f, img_shape,
-        (filter_h, filter_w), padding, strides, imgs_d._data
-    )
-    return imgs_d
+    def output_shape(self, imgs_shape, n_filters, filter_shape):
+        b, _, img_h, img_w = imgs_shape
+        out_shape = ((img_h + 2*self.padding[0] - filter_shape[0])
+                     / self.strides[0] + 1,
+                     (img_w + 2*self.padding[1] - filter_shape[1])
+                     / self.strides[1] + 1)
+        return (b, n_filters) + out_shape
--- cudarray/nnet/pool.py
+++ cudarray/nnet/pool.py
@@ -3,51 +3,60 @@ import cudarray as ca
 from ..cudarray_wrap import nnet as wrap
 
 
-def pool_b01(imgs, win_shape, padding, strides, method='max', poolout=None,
-             mask=None):
-    img_h, img_w = imgs.shape[-2:]
-    n_imgs_shape = imgs.shape[:-2]
-    poolout_h = (img_h + 2*padding[0] - win_shape[0])/strides[0] + 1
-    poolout_w = (img_w + 2*padding[1] - win_shape[1])/strides[1] + 1
-    poolout_shape = n_imgs_shape + (poolout_h, poolout_w)
+class PoolB01(object):
+    def __init__(self, win_shape, padding, strides, method='max'):
+        self.win_shape = win_shape
+        self.padding = padding
+        self.strides = strides
+        if method not in ['max']:
+            raise ValueError('invalid pooling method')
+        self.mask = None
 
-    if poolout is None:
-        poolout = ca.empty(poolout_shape, dtype=imgs.dtype)
-    else:
-        if poolout_shape != poolout.shape:
-            raise ValueError('poolout.shape does not match result')
-        if imgs.dtype != poolout.dtype:
-            raise ValueError('dtype mismatch')
+    def fprop(self, imgs, poolout=None):
+        poolout_shape = self.output_shape(imgs.shape)
+        if poolout is None:
+            poolout = ca.empty(poolout_shape, dtype=imgs.dtype)
+        else:
+            if poolout_shape != poolout.shape:
+                raise ValueError('poolout.shape does not match result')
+            if imgs.dtype != poolout.dtype:
+                raise ValueError('dtype mismatch')
 
-    if mask is None:
-        mask = ca.empty(poolout_shape, dtype=np.dtype('int32'))
-    else:
-        if mask.shape != poolout.shape:
-            raise ValueError('poolout.shape does not match result')
-        if mask.dtype != np.dtype('int32'):
-            raise ValueError('dtype mismatch')
+        if self.mask is None or self.mask.shape != poolout_shape:
+            self.mask = ca.empty(poolout_shape, dtype=np.dtype('int32'))
 
-    n_imgs = np.prod(n_imgs_shape)
-    wrap._max_pool_b01(imgs._data, n_imgs, (img_h, img_w), win_shape, padding,
-                       strides, poolout._data, mask._data)
-    return poolout, mask
+        img_shape = imgs.shape[-2:]
+        n_imgs = np.prod(imgs.shape[:-2])
+        wrap._max_pool_b01(
+            imgs._data, n_imgs, img_shape, self.win_shape, self.padding,
+            self.strides, poolout._data, self.mask._data
+        )
+        return poolout
 
+    def bprop(self, img_shape, poolout_d, imgs_d=None):
+        n_imgs_shape = poolout_d.shape[:-2]
+        imgs_shape = n_imgs_shape + img_shape
 
-def pool_b01_bprop(poolout_d, mask, img_shape, win_shape, padding, strides,
-                   method='max', imgs_d=None):
-    img_h, img_w = img_shape
-    n_imgs_shape = poolout_d.shape[:-2]
-    imgs_shape = n_imgs_shape + (img_h, img_w)
+        if imgs_d is None:
+            imgs_d = ca.empty(imgs_shape, dtype=poolout_d.dtype)
+        else:
+            if imgs_d.shape != imgs_d_shape:
+                raise ValueError('poolout.shape does not match result')
+            if imgs_d.dtype != poolout_d.dtype:
+                raise ValueError('dtype mismatch')
 
-    if imgs_d is None:
-        imgs_d = ca.empty(imgs_shape, dtype=poolout_d.dtype)
-    else:
-        if imgs_d.shape != imgs_d_shape:
-            raise ValueError('poolout.shape does not match result')
-        if imgs_d.dtype != poolout_d.dtype:
-            raise ValueError('dtype mismatch')
+        n_imgs = np.prod(n_imgs_shape)
+        wrap._max_pool_b01_bprop(
+            poolout_d._data, self.mask._data, n_imgs, img_shape,
+            self.win_shape, self.padding, self.strides, imgs_d._data
+        )
+        return imgs_d
 
-    n_imgs = np.prod(n_imgs_shape)
-    wrap._max_pool_b01_bprop(poolout_d._data, mask._data, n_imgs, img_shape,
-                             win_shape, padding, strides, imgs_d._data)
-    return imgs_d
+    def output_shape(self, imgs_shape):
+        n_imgs_shape = imgs_shape[:-2]
+        img_h, img_w = imgs_shape[-2:]
+        out_shape = ((img_h + 2*self.padding[0] - self.win_shape[0])
+                     / self.strides[0] + 1,
+                     (img_w + 2*self.padding[1] - self.win_shape[1])
+                     / self.strides[1] + 1)
+        return n_imgs_shape + out_shape
