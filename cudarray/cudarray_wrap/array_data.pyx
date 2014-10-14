e50f0948f4540475b881d34f6ca3ab71ff0ac1eb
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2014-10-14 20:38:21 +0200

    added support for ArrayData views
    ArrayData can now be initialized with another ArrayData object as
    parameter. This creates a view to the existing data instead of
    allocating a new array.

--- cudarray/cudarray_wrap/array_data.pyx
+++ cudarray/cudarray_wrap/array_data.pyx
@@ -1,14 +1,18 @@
 cimport numpy as np
-
 from ..cuda_wrap.cudart cimport *
 from .array_data cimport ArrayData
 
 
 cdef class ArrayData:
-    def __init__(self, size_t size, np.dtype dtype, np.ndarray np_data=None):
+    def __init__(self, size_t size, np.dtype dtype, np.ndarray np_data=None,
+                 ArrayData owner=None, unsigned int offset=0):
         self.dtype = dtype
         self.nbytes = size*dtype.itemsize
-        cudaCheck(cudaMalloc(&self.dev_ptr, self.nbytes))
+        self.owner = owner
+        if owner is None:
+            cudaCheck(cudaMalloc(&self.dev_ptr, self.nbytes))
+        else:
+            self.dev_ptr = owner.dev_ptr + offset*dtype.itemsize
         if np_data is not None:
             cudaCheck(cudaMemcpyAsync(self.dev_ptr, np.PyArray_DATA(np_data),
                                       self.nbytes, cudaMemcpyHostToDevice))
@@ -19,7 +23,8 @@ cdef class ArrayData:
         return np_array
 
     def __dealloc__(self):
-        cudaFree(self.dev_ptr)
+        if self.owner is None:
+            cudaFree(self.dev_ptr)
 
     @property
     def data(self):
