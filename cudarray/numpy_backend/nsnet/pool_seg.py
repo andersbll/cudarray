829391e4ba36d079b6e9b4e01771f1e2caa77084
lre <lre@2bm.com>
2015-01-15 14:23:43 +0100

    ) error

--- cudarray/numpy_backend/nsnet/pool_seg.py
+++ cudarray/numpy_backend/nsnet/pool_seg.py
@@ -55,8 +55,8 @@ class PoolB01(object):
         if output_index == None:
             f_out = input_index.shape[0] * self.strides[0] * self.strides[1] 
             index_h, index_w = input_index.shape[-2:]
-            out_shape = ((index_h /self.strides[0] ,
-                         (index_w /self.strides[1])
+            out_shape = ((index_h /self.strides[0]) ,
+                         (index_w /self.strides[1]))
             output_index = ca.empty(((f_out,)+out_shape), dtype=input_index.dtype)
             output_index -= 1
 
@@ -72,6 +72,6 @@ class PoolB01(object):
         c_in = imgs_shape[1]
         f_out = f_in * self.strides[0] * self.strides[1] 
         img_h, img_w = imgs_shape[-2:]
-        out_shape = ((img_h / self.strides[0] ,
-                     (img_w / self.strides[1])
+        out_shape = ((img_h / self.strides[0]) ,
+                     (img_w / self.strides[1]))
         return (f_out, c_in) + out_shape
