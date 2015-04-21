b28f8721d69a0599a23f74bbd791a1ec93d5b584
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2015-04-21 16:12:45 +0200

    fixed compiler warning

--- src/nnet/cudnn.cpp
+++ src/nnet/cudnn.cpp
@@ -82,7 +82,7 @@ template <typename T>
 ConvBC01CuDNN<T>::ConvBC01CuDNN(int pad_y, int pad_x, int stride_y,
     int stride_x) : pad_y(pad_y), pad_x(pad_x), stride_y(stride_y),
     stride_x(stride_x), n_imgs(0), n_channels(0), n_filters(0), img_h(0),
-    img_w(0), filter_h(0), filter_w(0), workspace_size(0), workspace(NULL) {
+    img_w(0), filter_h(0), filter_w(0), workspace(NULL), workspace_size(0) {
   CUDNN_CHECK(cudnnCreateTensorDescriptor(&imgs_desc));
   CUDNN_CHECK(cudnnCreateTensorDescriptor(&convout_desc));
   CUDNN_CHECK(cudnnCreateFilterDescriptor(&filters_desc));
