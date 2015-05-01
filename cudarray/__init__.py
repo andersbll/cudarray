6851b5816bd848dd772fd0722b009d26800019d4
Anders Boesen Lindbo Larsen <anders.bll@gmail.com>
2015-05-01 15:53:26 +0200

    revised back-end initialization and introduced _backend variable

--- cudarray/__init__.py
+++ cudarray/__init__.py
@@ -1,11 +1,27 @@
 import os
 import logging
 logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)-8s %(message)s',
+)
 
 _gpu_id = int(os.getenv('CUDARRAY_DEVICE', '0'))
 
-if 'CUDARRAY_BACKEND' not in os.environ:
-    # If no backend specified, try CUDA with Numpy fall-back.
+_backend = os.getenv('CUDARRAY_BACKEND')
+if _backend is not None:
+    _force_backend = True
+    _backend = _backend.lower()
+    _valid_backends = ['numpy', 'cuda']
+    if _backend not in _valid_backends:
+        raise RuntimeError('Invalid back-end "%s" specified. Valid options '
+                           'are: %s' % (_backend, _valid_backends))
+else:
+    # If no back-end specified, try CUDA with NumPy fall-back.
+    _force_backend = False
+    _backend = 'cuda'
+
+if _backend == 'cuda':
     try:
         from .base import *
         from .cudarray import *
@@ -16,31 +32,16 @@ if 'CUDARRAY_BACKEND' not in os.environ:
         from . import nnet
         from . import batch
         wrap.cudart.initialize(_gpu_id)
-        logger.info('CUDArray: Using CUDA back-end.')
     except:
-        from .numpy_backend import *
-        logger.info('CUDArray: Using Numpy back-end.')
-else:
-    backend = os.getenv('CUDARRAY_BACKEND', 'numpy').lower()
-    if backend == 'numpy':
-        from .numpy_backend import *
-    elif backend == 'cuda':
-        try:
-            from .base import *
-            from .cudarray import *
-            from .linalg import *
-            from .elementwise import *
-            from .reduction import *
-            from . import random
-            from . import nnet
-            from . import batch
-            wrap.cudart.initialize(_gpu_id)
-        except:
+        if _force_backend:
             logger.error('CUDArray: Failed to load CUDA back-end.')
             raise
-    else:
-        valid_backends = ['numpy', 'cuda']
-        raise ValueError('Invalid back-end "%s" specified.' % backend
-                         + ' Valid options are: ' + str(valid_backends))
+        else:
+            # Try NumPy instead
+            _backend = 'numpy'
+
+if _backend == 'numpy':
+    from .numpy_backend import *
+
 
 __version__ = '0.1.dev'
